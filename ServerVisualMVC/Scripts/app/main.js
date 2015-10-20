df6b35d6202619b16fc31b12ce51e125fb56852b
Desting <Nik_zd@hotmail.com>
2015-10-20 15:37:54 +0200

    Breakthrough with Angular.
    Can now retrieve data via api

---
+++ ServerVisualMVC/Scripts/app/main.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+
+
+
+    .controller('customersCtrl', function ($scope, $http) {
+        $http.get("http://www.w3schools.com/angular/customers.php")
+        .success(function (response) { $scope.names = response.records; });
+    })
+
+    .controller('planetController', function($scope, $http) {
+        // Simple GET request example:
+        $http({
+            method: 'GET',
+            url: 'http://localhost:60883/home/entities'
+        }).then(function successCallback(response) {
+            $scope.servers = response.data;
+            // this callback will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    })
+
+    ;;
+
+
+    
+
+    
+
+})();
